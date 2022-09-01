e8e7b5ce2dc05258c3e5d469805615279c552275
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2022-09-01 14:46:51 -0400

    set up dotenv

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import React, { useState, useEffect } from "react";
 import "./App.css";
+dotenv.config();
 
 function App() {
+  // States
   const [weather, setWeather] = useState({});
   const [locations, setLocations] = useState("toronto");
   const [photos, setPhotos] = useState([]);
+
+  // Method: if
+  function ifClicked() {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${locations}&APPID=${process.env.REACT_APP_WEATHER}&units=metric`
+    )
+      .then((res) => {
+        if (res.ok) {
+          console.log(res.status);
+          return res.json();
+        } else {
+          if (res.status === 404) {
+            return alert("Oops, there seems to be an error!(wrong location)");
+          }
+          alert("Oops, there seems to be an error!");
+          throw new Error("You have an error");
+        }
+      })
+      .then((object) => {
+        setWeather(object);
+        console.log(weather);
+      })
+      .catch((error) => console.log(error));
+    fetch(
+      `https://api.unsplash.com/search/photos?query=${locations}&client_id={API_KEY_FOR_UNSPLASH}`
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("You made a mistake");
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        setPhotos(data?.results[0]?.urls?.raw);
+      })
+      .catch((error) => console.log(error));
+  }
 
   return <div>Making weather app, please standby...</div>;
 }
