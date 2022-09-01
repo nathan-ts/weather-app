ce21f0b0ddab657df8bf0fe8b0e383f076dd9e10
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2022-09-01 18:06:36 -0400

    consolidate photo and photographer credits into one state

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
   // States
   const [weather, setWeather] = useState({});
   const [locations, setLocations] = useState("toronto");
-  const [photos, setPhotos] = useState([]);
-  const [photographer, setPhotographer] = useState([]);
+  const [photo, setPhoto] = useState([]);
+  const [cloud, setCloud] = useState([]);
+  // const [photographer, setPhotographer] = useState([]);
 
 
   // Run getWeatherImage once on component load
@@ -50,28 +51,54 @@ function App() {
       })
       .then((data) => {
         console.log('Unsplash API returned:', data);
-        setPhotos(data?.results[0]?.urls?.raw);
-        setPhotographer({
+        setPhoto({
+          img: data?.results[0]?.urls?.raw,
           name: data?.results[0]?.user?.name,
           username: data?.results[0]?.user?.username,
         });
+        // setPhotographer({
+        //   name: data?.results[0]?.user?.name,
+        //   username: data?.results[0]?.user?.username,
+        // });
       })
       .catch((error) => console.log(error));
+    // fetch(
+    //   `https://api.unsplash.com/search/photos?query=cloud&client_id=${process.env.REACT_APP_UNSPLASH}`
+    // )
+    //   .then((res) => {
+    //     if (res.ok) {
+    //       return res.json();
+    //     } else {
+    //       throw new Error("Error occurred when fetching image");
+    //     }
+    //   })
+    //   .then((data) => {
+    //     console.log('Unsplash API returned:', data);
+    //     setCloud(data?.results[0]?.urls?.raw);
+    //     setPhotographer({
+    //       name: data?.results[0]?.user?.name,
+    //       username: data?.results[0]?.user?.username,
+    //     });
+    //   })
+    //   .catch((error) => console.log(error));
   }
 
   return (
     <div className={`app 
       flex flex-col justify-center items-center
       min-h-screen w-screen
-      bg-slate-400
+      bg-gradient-to-br from-slate-300 to-blue-400
     `}>
       <div className="wrapper
-        p-6
+        p-6 m-12
         border-0
         rounded-xl
         flex flex-col
         w-2/5 h-auto
-        shadow-lg 
+        max-h-fit
+        shadow-2xl
+        bg-slate-300/25
+        backdrop-blur-2xl
       ">
         <div className="search">
           <input
@@ -93,24 +120,26 @@ function App() {
             Search Location
           </button>
         </div>
-        <div className="app__data">
+        {/* <div className="app__data">
           <p className="temperature
             m-1.5 text-slate-800
           ">
             Current Temparature: {Math.round(weather?.main?.temp)} &deg;C
           </p>
-        </div>
+        </div> */}
         <WeatherData weather={weather} />
         <img className="app__image
-          w-auto rounded-lg
+          w-auto 
+          rounded-lg
+          saturate-150
           " 
-          src={photos} alt="" 
+          src={photo?.img} alt="" 
         />
-        <div className="credits">
+        <div className="credits text-base text-slate-700">
           <p>
             Photo by&nbsp;
-            <Link url={`https://unsplash.com/@${photographer?.username}?utm_source=weather_app&utm_medium=referral`}>
-              {photographer?.name}
+            <Link url={`https://unsplash.com/@${photo?.username}?utm_source=weather_app&utm_medium=referral`}>
+              {photo?.name}
             </Link>
             &nbsp;on&nbsp;
             <Link url={`https://unsplash.com/?utm_source=weather_app&utm_medium=referral`}>
--- src/components/Link.js
+++ src/components/Link.js
@@ -3,7 +3,6 @@ export default function Link(props) {
     <a target="_blank" rel="noreferrer"
       className="
         underline underline-offset-4
-        text-zinc-800
       " 
       href={props.url}
     >
