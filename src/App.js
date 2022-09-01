a490af8560461453d9d1ee85ada4d155e2803b5b
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2022-09-01 14:39:27 -0400

    add default states

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [weather, setWeather] = useState({});
+  const [locations, setLocations] = useState("toronto");
+  const [photos, setPhotos] = useState([]);
+
   return <div>Making weather app, please standby...</div>;
 }
 
