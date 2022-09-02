186be36d84db7d906b1f48a9a8e4fca6b7d3b9bd
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2022-09-02 16:40:51 -0400

    update weather styling with font awesome

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,6 +1,12 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTemperatureThreeQuarters, faAngleDown } from '@fortawesome/free-solid-svg-icons'
+
 import Map from "./Map";
 
 export default function WeatherData(props) {
+  
+
   // Conditional return based on whether or not weather data is loaded
   const temperature = props.weather?.main?.temp;
   if (typeof temperature !== 'number' || Number.isNaN(temperature)) {
@@ -11,14 +17,21 @@ export default function WeatherData(props) {
   }
   return (
     <div className="app-data
-      flex flex-row justify-center
+      py-6 px-auto w-full
+      flex flex-row justify-between items-center
     ">
+      <div className="padding p-4">
+      </div>
       <p className="temperature
-        py-6 px-auto
-        text-black text-3xl
+        text-black text-2xl
       ">
-        Current Temperature: {Math.round(temperature)} &deg;C
+        <FontAwesomeIcon icon={faTemperatureThreeQuarters} />
+        : {Math.round(temperature)}&deg;C&nbsp;
       </p>
+      <FontAwesomeIcon icon={faAngleDown} className="weather-open-arrow
+        text-black text-2xl" 
+        // onClick={}
+      />
     </div>
   )
 }
