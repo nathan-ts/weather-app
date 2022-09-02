02076d1858d8ade888e8ab7bd01a0ff43de8783f
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2022-09-02 02:24:46 -0400

    add conditional render to ensure NaN temp isn't shown

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,12 +1,18 @@
 export default function WeatherData(props) {
+  if (typeof props.weather?.main?.temp === 'number' && !Number.isNaN(props.weather?.main?.temp)) {
+    return (
+      <div className="app__data">
+        <p className="temperature
+          py-2
+          text-black text-xl
+        ">
+          Current Temparature: {Math.round(props.weather?.main?.temp)} &deg;C
+        </p>
+      </div>
+    )
+  }
   return (
-    <div className="app__data">
-      <p className="temperature
-        py-2
-        text-black text-xl
-      ">
-        Current Temparature: {Math.round(props.weather?.main?.temp)} &deg;C
-      </p>
-    </div>
+    <>
+    </>
   )
 }
